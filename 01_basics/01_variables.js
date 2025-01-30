34324359ab95428594974f711e655b0cff5163fa
KAUSHAMBI ROY <kaushambiroy90@gmail.com>
2025-01-30 20:37:05 +0530

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144543      // constant variable is declared using the keyword "const" and it cannot be modified in further code
+let accountemail = "kaushambiroy90@gmail.com"   // let keyword use to assign a space for the variable and can be modified in the further code
+var accountpassword = "1234" //////same as let keyword
+accountcity = "jaipur"   //js allows such declaration of variable without any keyword
+let accounstate;  
+
+// accountId = 2  //note allowed to rewrite a constant
+
+
+/*
+prefer not to use "var"
+because of its issue in block scope and functional scope
+
+*/
+
+accountemail = "kaushambi.com"
+ accountpassword = "90878"
+accountcity = "kolkata"
+
+
+console.log(accountId);
+console.table([accountId,accountemail,accountpassword,accountcity,accounstate]);
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version of js
+
+// alert(3*3) //we r using nodejs, not browser so this gives error if run in the terminal
+
+console.log(3+3);
+
+// code readability should be high with proper indentation and spacing
+
+console.log("hitesh");
+
+let name = "hitesh"  // string datatype
+let age = 21    // int datatype
+let isLoggedIn = false    //boolean data type
+
+
+/* types of datatypes
+
+number => ranges between 2 to pwer 23
+bigint 
+string => ""
+boolean => true/false
+null => standalone value
+undefined => value not yet assigned
+symbol => used to find uniqueness
+
+
+// object
+
+*/
+
+console.log(typeof undefined); // output is undefined
+console.log(typeof null);   //output is object
+console.log(typeof age); // output is number
+console.log(typeof "hitesh");  //output is string
